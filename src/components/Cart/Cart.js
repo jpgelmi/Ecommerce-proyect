a16b22fed3a2e81603a42699d217c6504a03ba58
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-11-27 21:41:09 -0300

    commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 import Title from "../Title";
-import CardColumns from "./Cart"
+import CartColumns from "./CartColumns"
+import EmptyCart from "./EmptyCart"
+import {ProductConsumer} from "../../contex"
 
 export default class Cart extends Component {
     render() {
         return (
             <section>
-                <Title name = "your" title = "cart"/>
-                <CardColumns />
+                <ProductConsumer>
+                    {value => {
+                        const {cart} = value;
+                        if(cart.length > 0){
+                            return(
+                                <React.Fragment>
+                                    <Title name = "your" title = "cart"/>
+                                    <CartColumns />
+                                </React.Fragment>
+                            )
+                        }
+                        else{
+                            return(
+                                <EmptyCart />
+                            )
+                        }
+                    }}
+                </ProductConsumer>
             </section>
         )
     }
