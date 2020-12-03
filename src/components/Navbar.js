5e47c6be136f1cd9bdac772b51fc4162d8fe87a5
Juan Pablo Gelmi <jpgelmi@gmail.com>
2020-12-02 23:35:19 -0300

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react' 
 import {Link} from "react-router-dom";
 import logo from "../logo.png"
 import styled from "styled-components"
